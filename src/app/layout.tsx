60ef7acc2be982cea3972fa12deca9fc35d5a592
Olowodarey <olowodarey8@gmail.com>
2026-03-25 15:06:56 +0100

    feat: integrate Toaster into app layout
    - Add Toaster component to root layout
    - Enable toast notifications across entire application
    - Import and render Toaster after DashboardShell

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "./components/providers/QueryProvider";
 import { DashboardShell } from "./components/global_ui/DashboardShell";
+import { Toaster } from "./components/ui/Toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -65,6 +66,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <QueryProvider>
           <DashboardShell>{children}</DashboardShell>
+          <Toaster />
         </QueryProvider>
       </body>
     </html>
